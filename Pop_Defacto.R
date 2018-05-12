3dcb686f3490d031e2798abe4417b349bc2d9cef
mrsensible <hs621@cam.ac.uk>
2018-05-12 18:34:38 +0900

    update codes and base R maps

--- Pop_Defacto.R
+++ Pop_Defacto.R
@@ -4,17 +4,15 @@ options(scipen = 10000, width = 10)
 
 library(readr)
 jippop <- read_csv("pop_jip_20180504.csv")
-colnames(jippop) <- c('date','hour','admincd','totalpop',
+colnames(jippop) <- c('date','hour','admincd', 'blockcd','totalpop',
                       'M00','M10','M15','M20','M25','M30','M35',
                       'M40','M45','M50','M55','M60','M65','M70',
                       'F00','F10','F15','F20','F25','F30','F35',
                       'F40','F45','F50','F55','F60','F65','F70')
 for(i in 1:length(jippop)){
   jippop[[i]] <- as.numeric(jippop[[i]])
-  
 }
 jippop <- jippop[order(jippop$hour, jippop$blockcd),]
-
 
 adminpop <- read_csv("pop_admin_20180504.csv")
 colnames(adminpop) <- c('date','hour','admincd','totalpop',
@@ -24,7 +22,6 @@ colnames(adminpop) <- c('date','hour','admincd','totalpop',
                         'F40','F45','F50','F55','F60','F65','F70')
 for(i in 1:length(adminpop)){
   adminpop[[i]] <- as.numeric(adminpop[[i]])
-  
 }
 adminpop <- adminpop[order(adminpop$hour, adminpop$admincd),]
 
@@ -32,7 +29,9 @@ adminpop <- adminpop[order(adminpop$hour, adminpop$admincd),]
 ###########################
 #-- Add code match book --#
 ###########################
-codebook <- read.csv("adm_code_match.csv")
+codebook <- read.csv("adm_code_match.csv") # Windows
+codebook <- read.csv("adm_code_match.csv", fileEncoding = "CP949", encoding = "UTF-8") # MacOS
+
 Adminpop <- merge(adminpop, codebook, by.x = "admincd", by.y = "H_DNG_CD")
 
 
@@ -41,17 +40,39 @@ Adminpop <- merge(adminpop, codebook, by.x = "admincd", by.y = "H_DNG_CD")
 #########################
 library(rgdal)
 path <- "D:/Dropbox (Cambridge University)/2018_Cambridge/[Database]/Boundary/Sudo/"
+path <- "~/Dropbox (Cambridge University)/2018_Cambridge/[Database]/Boundary/Sudo/"
+
 jip   <- readOGR(paste0(path, 'Seoul_Jipgegu.shp'))
 admin <- readOGR(paste0(path,'Seoul_Dong_Admin.shp'))
 admin@data <- admin@data[,4:7]
 admin@data[[1]] <- as.numeric(as.character(admin@data[[1]]))
 admin@data <- admin@data[order(admin@data$DONG_CODE),]
 
-
 jip@data   <- merge(jip@data, jippop[1:5], by.x = "TOT_OA_CD", by.y = "blockcd")
 admin@data <- merge(admin@data, Adminpop, by.x = "DONG_CODE", by.y = 'H_SDNG_CD')
 
-#writeOGR(admin, "admin",driver="ESRI Shapefile")
+###############
+ad1 <- admin
+ad2 <- admin
+ad3 <- admin
 
+ad1@data <- ad1@data[ad1@data$hour == 0,] # Sampling hours equvalent to 0
+ad2@data <- ad2@data[ad2@data$hour == 16,] # Sampling hours equvalent to 16
+ad3@data <- ad3@data[ad3@data$hour == 20,] # Sampling hours equvalent to 20
 
-##Leaflet Package ?´?š©?•´?•¼?•¨
+#######################
+library(RColorBrewer)
+brewer.pal.info
+
+cols <- brewer.pal(5, "Oranges")
+brks <- c(10000, 20000, 30000, 40000, 50000)#, 60000, 70000, 80000)
+
+gs1 <- cols[findInterval(ad1@data$totalpop, vec = brks)]
+gs2 <- cols[findInterval(ad2@data$totalpop, vec = brks)]
+gs3 <- cols[findInterval(ad3@data$totalpop, vec = brks)]
+
+plot(ad1, col = gs1)
+plot(ad2, col = gs2)
+plot(ad3, col = gs3)
+
+##Leaflet Package 
