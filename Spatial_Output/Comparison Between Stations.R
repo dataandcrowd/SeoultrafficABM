8b8e8d71dfe88e8c5fa5b1a5e667a85bc7207072
dataandcrowd <shinhyesop@gmail.com>
2022-01-20 22:13:36 +0000

    Update Comparison Between Stations.R

--- Spatial_Output/Comparison Between Stations.R
+++ Spatial_Output/Comparison Between Stations.R
@@ -5,10 +5,18 @@ library(rstatix)
 pm10 <- fread("pm10_comparison btw stations.csv")
 
 pm10 %>% 
+  select(back_pm10_sample, JongnoKerb_p, Sejong_p) %>% 
   pivot_longer(cols = everything(),
                names_to = "Station",
                values_to = "PM10") %>% 
   group_by(Station) %>% 
   get_summary_stats()
 
+
+pm10 %>% 
+  select(back_pm10_sample, JongnoKerb_p, Sejong_p) %>% 
+  mutate(ratio_Jongno = round(JongnoKerb_p / back_pm10_sample, 2),
+         ratio_Sejong = round(Sejong_p / back_pm10_sample, 2)) %>% 
+  slice(-1) %>% # first tick is the setup
+  get_summary_stats(ratio_Jongno, ratio_Sejong)
 
